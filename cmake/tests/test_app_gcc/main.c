5d08855c3f149c731ca6f07c7e8e635d360e8f0d
itseddie <its.eddie@icloud.com>
2025-05-25 01:54:28 -0700

    Test Project Runs
    - Modified the test project to run on an STM32H750B-DK.
      - Project blinks the red and green LEDs.

--- cmake/tests/test_app_gcc/main.c
+++ cmake/tests/test_app_gcc/main.c
@@ -1,7 +1,201 @@
+#include "stm32h7xx_hal.h"
+
 // Provide _init() implementation to satisfy __libc_init_array()
 void _init(void) {}
 
+#define RED_LED_PIN GPIO_PIN_2
+#define GREEN_LED_PIN GPIO_PIN_13
+
+GPIO_InitTypeDef red_led_gpio_cfg = {
+    .Pin = RED_LED_PIN,
+    .Mode = GPIO_MODE_OUTPUT_PP,
+    .Pull = GPIO_NOPULL,
+    .Speed = GPIO_SPEED_FREQ_MEDIUM,
+    .Alternate = 0,
+};
+
+GPIO_InitTypeDef green_led_gpio_cfg = {
+    .Pin = GREEN_LED_PIN,
+    .Mode = GPIO_MODE_OUTPUT_PP,
+    .Pull = GPIO_NOPULL,
+    .Speed = GPIO_SPEED_FREQ_MEDIUM,
+    .Alternate = 0,
+};
+
+static void MPU_Config(void)
+{
+    MPU_Region_InitTypeDef MPU_InitStruct;
+
+    /* Disable the MPU */
+    HAL_MPU_Disable();
+
+    /* Configure the MPU as Strongly ordered for not defined regions */
+    MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+    MPU_InitStruct.BaseAddress = 0x00;
+    MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
+    MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
+    MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+    MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+    MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+    MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+    MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+    MPU_InitStruct.SubRegionDisable = 0x87;
+    MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+
+    HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+    /* Enable the MPU */
+    HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+}
+
+static void SystemClock_Config(void)
+{
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
+    HAL_StatusTypeDef ret = HAL_OK;
+
+    /* The voltage scaling allows optimizing the power consumption when the device is
+       clocked below the maximum system frequency, to update the voltage scaling value
+       regarding system frequency refer to product datasheet.  */
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+
+    /* Enable HSE Oscillator and activate PLL with HSE as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+    RCC_OscInitStruct.CSIState = RCC_CSI_OFF;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+
+    RCC_OscInitStruct.PLL.PLLM = 5;
+    RCC_OscInitStruct.PLL.PLLN = 160;
+    RCC_OscInitStruct.PLL.PLLFRACN = 0;
+    RCC_OscInitStruct.PLL.PLLP = 2;
+    RCC_OscInitStruct.PLL.PLLR = 2;
+    RCC_OscInitStruct.PLL.PLLQ = 4;
+
+    RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+    RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
+    ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+    if(ret != HAL_OK)
+    {
+        // TODO: Add error handling here
+    }
+
+    /* PLL3 for USB Clock */
+    PeriphClkInitStruct.PLL3.PLL3M = 25;
+    PeriphClkInitStruct.PLL3.PLL3N = 336;
+    PeriphClkInitStruct.PLL3.PLL3P = 2;
+    PeriphClkInitStruct.PLL3.PLL3R = 2;
+    PeriphClkInitStruct.PLL3.PLL3Q = 7;
+
+    PeriphClkInitStruct.PLL3.PLL3VCOSEL = RCC_PLL3VCOMEDIUM;
+    PeriphClkInitStruct.PLL3.PLL3RGE = RCC_PLL3VCIRANGE_0;
+
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USB;
+    PeriphClkInitStruct.UsbClockSelection = RCC_USBCLKSOURCE_PLL3;
+    HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
+
+    /* Select PLL as system clock source and configure  bus clocks dividers */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_D1PCLK1 | RCC_CLOCKTYPE_PCLK1 | \
+                                 RCC_CLOCKTYPE_PCLK2  | RCC_CLOCKTYPE_D3PCLK1);
+
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+    RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+    ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
+    if(ret != HAL_OK)
+    {
+        // TODO: Add error handling here
+    }
+
+    /*
+     Note : The activation of the I/O Compensation Cell is recommended with communication  interfaces
+             (GPIO, SPI, FMC, QSPI ...)  when  operating at  high frequencies(please refer to product datasheet)
+             The I/O Compensation Cell activation  procedure requires :
+           - The activation of the CSI clock
+           - The activation of the SYSCFG clock
+           - Enabling the I/O Compensation Cell : setting bit[0] of register SYSCFG_CCCSR
+    */
+
+    /*activate CSI clock mondatory for I/O Compensation Cell*/
+    __HAL_RCC_CSI_ENABLE() ;
+
+    /* Enable SYSCFG clock mondatory for I/O Compensation Cell */
+    __HAL_RCC_SYSCFG_CLK_ENABLE() ;
+
+    /* Enables the I/O Compensation Cell */
+    HAL_EnableCompensationCell();
+}
+
+/**
+  * @brief  CPU L1-Cache enable.
+  * @param  None
+  * @retval None
+  */
+static void CPU_CACHE_Enable(void)
+{
+    /* Enable I-Cache */
+    SCB_EnableICache();
+
+    /* Enable D-Cache */
+    SCB_EnableDCache();
+}
+
 int main(void)
 {
+    MPU_Config();
+
+    /* Enable the CPU Cache */
+    CPU_CACHE_Enable();
+
+    /* STM32H7xx HAL library initialization:
+         - Configure the Systick to generate an interrupt each 1 msec
+         - Set NVIC Group Priority to 4
+         - Low Level Initialization
+       */
+    HAL_Init();
+
+    /* Configure the system clock to 400 MHz */
+    SystemClock_Config();
+
+    __HAL_RCC_GPIOI_CLK_ENABLE();
+    __HAL_RCC_GPIOJ_CLK_ENABLE();
+
+    HAL_GPIO_Init(GPIOJ, &red_led_gpio_cfg);
+    HAL_GPIO_Init(GPIOI, &green_led_gpio_cfg);
+
+    while (1){
+        for (int i = 0; i < 100000000; ++i) {
+            // Do nothing
+        }
+
+        HAL_GPIO_TogglePin(GPIOJ, RED_LED_PIN);
+
+        for (int i = 0; i < 200000000; ++i) {
+            // Do nothing
+        }
+
+        HAL_GPIO_TogglePin(GPIOI, GREEN_LED_PIN);
+    }
+
     return 0;
+}
+
+void HardFault_Handler(void)
+{
+    __asm volatile("BKPT #0");
+    while (1);
+}
+
+void SysTick_Handler(void)
+{
+    HAL_IncTick();
 }
